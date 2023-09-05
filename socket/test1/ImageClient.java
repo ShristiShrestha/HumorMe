c4c3eccbaa9efdbf875ab1c44c066300a3c0c76b
Shristi Shrestha <sthashristi777@gmail.com>
2023-09-05 01:08:01 -0500

    adds server and client img transer

--- socket/test1/ImageClient.java
+++ socket/test1/ImageClient.java
@@ -6,22 +6,67 @@ import java.net.*;
 public class ImageClient {
     public static void main(String[] args) {
         String host = "localhost";
+        String imageName = "Koala.png";
         try {
             Socket socket = new Socket(host, 8008);
+            // read data from the server from this
             BufferedInputStream in = new BufferedInputStream(
                     socket.getInputStream());
+            // write data to the server using this
             PrintWriter out = new PrintWriter(new OutputStreamWriter(
                     socket.getOutputStream()));
 
             /* Detailed requirement below*/
 
             // Step one: send the picture name "Koala.jpg" to the server
+            out.println(imageName);
+            out.flush(); // ensure imageName is written immediately
+
             // Step two: write the response from the server to a local file "Koala-1.jpg";
+            while(true){
+                char serverData = (char) in.read();
+                DataInputStream reader = new DataInputStream(in);
+
+                if(serverData == 'M'){
+                    String charData = reader.readUTF();
+                    System.out.println("[CLIENT] server response: " + charData);
+                    break;
+                }
+                else {
+                    int byteSize = reader.readInt();
+                    byte[] byteData = new byte[byteSize];
+                    reader.read(byteData);
+                    System.out.println("[CLIENT] server responded with bytes: " + byteSize);
+
+                    String fileExt = imageName.split(".")[1];
+                    String outputImgName  = imageName.replace("." + fileExt, "-1." + fileExt);
+                    try{
+                        FileOutputStream fos = new FileOutputStream(outputImgName);
+                        fos.write(byteData);
+
+                        // close file and buffer readers
+                        fos.close();
+                        reader.close();
+
+                        System.out.println("[CLIENT] success saving server bytes to file: " +  outputImgName);
+                    }catch (Exception err){
+                        System.out.println("[CLIENT] client fail to save server bytes to file: " + outputImgName);
+                        err.printStackTrace();
+                    }
+                }
+            }
+
             // Step three: close all the input/output streams and socket.
-            // Step four: try to read the file "Desert-1.jpg" using any picture viewer. If you can view the picture correctly, your download of picture is correct.
+            out.close();
+            in.close();
+            socket.close();
+
+            // Step four: try to read the file "Desert-1.jpg" using any picture viewer.
+            // If you can view the picture correctly, your download of picture is correct.
 
             // run the client code again and try to send a wrong picture name "Koala-wrong.jpg"
-            // you will create a Koala-1.jpg again, this time use a normal text editor to open it, if you see "Sorry, no such picture", then your program is correct.
+            // you will create a Koala-1.jpg again, this time use a normal text editor to open it,
+            // if you see "Sorry, no such picture", then your program is correct.
 
         } catch (Exception e) {
             e.printStackTrace();
--- socket/test1/ImageServer.java
+++ socket/test1/ImageServer.java
@@ -7,20 +7,60 @@ public class ImageServer {
     public static void main(String[] args) {
         try {
             ServerSocket server = new ServerSocket(8008);
+            String imageName = "Koala.png";
 
             while (true) {
                 System.out.println("server is waiting for connection request from clients");
                 Socket s = server.accept();
                 BufferedReader in = new BufferedReader(new InputStreamReader(
                         s.getInputStream()));
-                DataOutputStream  out = new DataOutputStream (s.getOutputStream());
+                DataOutputStream out = new DataOutputStream (s.getOutputStream());
 
                 /* Detailed requirement below*/
 
-                //Step one: check the picture name sent from the client, if the picture name equals "Koala.jpg", go to step two, otherwise go to step three
-                //Step two, read the picture "Koala.jpg" from the local disk, and send the content back to the client.
-                //step three, then reply to the client with "Sorry, no such picture",
+                //Step one: check the picture name sent from the client,
+                String clientInput = null;
+                while((clientInput = in.readLine()) != null){
+                    System.out.println("[SERVER] received: " + clientInput);
+
+                    // if the picture name equals "Koala.jpg", go to step two, otherwise go to step three
+                    Boolean clientReqMatch = clientInput.equals(imageName);
+
+                    //Step two, read the picture "Koala.jpg" from the local disk, and send the content back to the client.
+                    if(clientReqMatch){
+                        // init image as file
+                        File imageFile = new File(imageName);
+
+                        if(!imageFile.exists()){
+                            throw new Exception("Failed to load image as File object.");
+                        }
+
+                        // read bytes from file
+                        FileInputStream fis = new FileInputStream(imageFile);
+                        byte[] imageData = new byte[(int) imageFile.length()];
+                        fis.read(imageData);
+                        fis.close();
+
+                        // send the image bytes
+                        out.writeInt(imageData.length);
+                        out.write(imageData);
+                        out.flush();
+
+                    }
+                    //step three, then reply to the client with "Sorry, no such picture",
+                    else{
+                        String resMessage = "Sorry, no such picture as " + clientInput;
+                        out.writeChar('M');
+                        out.writeUTF(resMessage);
+                        out.flush();
+                        break;
+                    }
+                }
+
                 //step four, close the input/output streams, close the socket.
+                in.close();
+                out.close();
+                s.close();
             }
         } catch (Exception e) { e.printStackTrace(); }
     }
