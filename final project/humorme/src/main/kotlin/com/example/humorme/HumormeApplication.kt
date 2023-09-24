104472a71c19c4bd9c1eee63eaff3aa2f2c6f1ce
Shristi Shrestha <sthashristi777@gmail.com>
2023-09-23 23:32:19 -0500

    adds proposal, init project

---
+++ final project/humorme/src/main/kotlin/com/example/humorme/HumormeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.humorme
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HumormeApplication
+
+fun main(args: Array<String>) {
+	runApplication<HumormeApplication>(*args)
+}
---
+++ final project/humorme/src/test/kotlin/com/example/humorme/HumormeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.humorme
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HumormeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
