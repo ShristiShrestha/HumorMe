fe6e5544fa2c4c32c0257c0607a903305895217a
Shristi Shrestha <sthashristi777@gmail.com>
2023-10-06 00:31:33 -0500

    use env in cors origins

--- server/README.md
+++ server/README.md
@@ -1,20 +1,13 @@
-# SERVER implementation for HumorMe!
+# SERVER implementation for HumorMe Server!
 
 ## Setup
 1. Download and run postgres server.
 2. Make necessary db connection credential changes on application.properties file, if necessary.
 3. Download and install jdk 11, maven 3.9.0 .
-4. To run the application with maven, execute `mvn package` followed by `java -jar -DPOSTGRES_DB_URL="localhost" target/online-scheduler-api-0.0.1-SNAPSHOT.jar`.
-5. To verify the application has started, visit [link](http://localhost:8080/actuator).
-
-## Run with docker
-1. Download and install docker.
-2. Execute the command `docker compose up -d`.
-3. Verify the application is running by visiting the [link](http://localhost:8080/actuator).
+4. To run the application with maven, execute `mvn package` followed by `java -jar -DPOSTGRES_DB_URL="localhost" target/humorme-api-0.0.1-SNAPSHOT.jar`.
 
 ## Swagger UI
 This is the link to swagger UI: http://localhost:8080/swagger-ui.html
-
 
 # Deploy in EC2 Ubuntu Container
 - copy jar file to the remote container:
--- server/src/main/java/com/fours/humorme/configs/CorsConfig.java
+++ server/src/main/java/com/fours/humorme/configs/CorsConfig.java
@@ -1,19 +1,26 @@
 package com.fours.humorme.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(allowedOrigins.split(","))
                         .allowedMethods("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("Content-Type", "Authorization", "Cookie", "*")
                         .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
